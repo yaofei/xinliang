346e87f5117059e90a923f83b82cef8d2c588f8a
iefoay <oiue@163.com>
2013-12-01 09:38:31 +0800

    v1

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,1 +1,18 @@
-$('.carousel').carousel();
+
+var url = document.location.href;
+
+$('#nav-top').find('a').each(function(index, node) {
+	if (node.href == url) {
+		$(node).parent().addClass('active');
+	}
+	else {
+		$(node).parent().removeClass("active");
+	}
+	
+});
+
+
+
+/*$(document).ready(function() {
+	$('h3').addClass('spacing');
+})*/
--- index.md
+++ index.md
@@ -2,14 +2,11 @@
 layout: index
 title: Hello!
 tagline: 我们是智融均策
+group: navigation
 css: [
-	assets/css/bootstrap-responsive.css,
-	assets/css/main.css
 ]
 js: [
-	assets/js/jquery-1.7.2.js,
-	assets/js/bootstrap.js,
-	assets/js/application.js
+	assets/js/index.js
 ]
 ---
 {% include JB/setup %}
